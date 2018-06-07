212c20bf0bd3213e63dfffa422a947114aa37b47
lyt2015 <lyt2015@users.nonreply.github.com>
2018-06-07 19:50:12 +0800

    Initial commit

---
+++ practice/mongodb-find.js
@@ -0,0 +1,54 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect(
+  'mongodb://localhost:27017/TodoApp',
+  (err, db) => {
+    if (err) {
+      return console.log('Unable to connect to MongoDB serer');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    /* db.collection('Todos')
+      .find({ _id: ObjectID('5b18736a0fe78e2cd49d8d04') })
+      .toArray()
+      .then(
+        docs => {
+          console.log('Todos: ');
+          console.log(JSON.stringify(docs, undefined, 2));
+        },
+        err => {
+          console.log('Unable to find todos. ', err);
+        }
+      );
+    */
+
+    /* db.collection('Todos')
+      .find()
+      .count()
+      .then(
+        count => {
+          console.log(`Todos count: ${count}`);
+        },
+        err => {
+          console.log('Unable to find todos. ', err);
+        }
+      ); */
+
+    db.collection('Users')
+      .find({ name: 'Zin' })
+      .toArray()
+      .then(
+        docs => {
+          console.log('Users:');
+          console.log(JSON.stringify(docs, undefined, 2));
+        },
+        err => {
+          console.log("Unable to find users' records ");
+        }
+      );
+
+    db.close();
+  }
+);
---
+++ practice/object-destruturing.js
@@ -0,0 +1,7 @@
+let user = { name: 'Zin', age: 300 };
+let { name } = user;
+console.log(name);
+
+user.name = 'Zin Liu';
+console.log(user);
+console.log(name);
