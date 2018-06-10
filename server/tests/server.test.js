cb528efc4bad7e65bd1b4fe0df2c927f0f63e1e7
lyt2015 <lyt2015@users.nonreply.github.com>
2018-06-10 15:20:14 +0800

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
 const todos = [
   {
+    _id: new ObjectID(),
     text: 'First test to do'
   },
   {
+    _id: new ObjectID(),
     text: 'Second test to do'
   }
 ];
@@ -81,6 +84,32 @@ describe('GET /todos', () => {
       .expect(res => {
         expect(res.body.docs.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe('GET /todos:id', () => {
+  it('should return a todo doc', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.doc.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 when todo is not found', done => {
+    request(app)
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 when the ID is invalid', done => {
+    request(app)
+      .get(`/todos/123`)
+      .expect(404)
       .end(done);
   });
 });
