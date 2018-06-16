64650f9664d0b30c5f5dee349a13186f4f2b52e0
lyt2015 <lyt2015@users.nonreply.github.com>
2018-06-16 15:00:57 +0800

    Fix POST /users error response

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post('/users', (req, res) => {
       res.header('x-auth', token).send(user);
     })
     .catch(err => {
-      res.status(400).send(err);
+      res.status(400).send();
     });
 });
 
